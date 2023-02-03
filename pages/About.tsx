229990609bf4148876f8415294077ce581317382
Krzysiek <supermax865@gmail.com>
2023-02-03 22:52:59 +0100

    added typography and css stuff to about me page

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,11 +1,34 @@
-'use client'
+import React from 'react';
+import { Button, ThemeProvider, Typography } from '@mui/material';
+import { responsiveFontSizes, createTheme } from '@mui/material';
+import styles from '../styles/About.module.scss';
 
-import React from 'react'
+let themeLeftSide = createTheme();
+themeLeftSide = responsiveFontSizes(themeLeftSide);
 
 const About = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <div className={styles.leftSide}>
+          <ThemeProvider theme={themeLeftSide}>
+            <Typography variant="h4" color="#00ADB5" className={styles.Title}>
+              About me
+            </Typography>
+            <Typography variant="subtitle1" color="white">
+              Eu id laboris sit dolor irure cupidatat laborum et voluptate non anim anim laborum
+              aliqua. Do voluptate anim enim voluptate officia ut ad. Sunt incididunt ut nisi mollit
+              magna dolor commodo elit. Reprehenderit ea sint qui nisi aliquip velit aliqua
+              consequat enim dolore. In consequat irure cupidatat voluptate ex labore sit aliquip ad
+              ex nulla dolore elit pariatur. Pariatur mollit elit labore dolor voluptate non velit
+              cupidatat aliquip culpa ullamco magna laboris. Sint sit reprehenderit aute ut.
+            </Typography>
+          </ThemeProvider>
+        </div>
+        <div className={styles.rightSide}>${'components'}</div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.scss';
-import { motion } from 'framer-motion';
 import React from 'react';
 import { Button, ThemeProvider, Typography } from '@mui/material';
 import { responsiveFontSizes, createTheme } from '@mui/material';
-import { color } from '@mui/system';
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
@@ -16,9 +11,7 @@ const Contact = () => {
     <div className={styles.main}>
       <div className={styles.part1}>
         <ThemeProvider theme={theme}>
-          <Typography
-            variant="h5"
-            sx={{ color: '#00ADB5', display: 'flex' }}>{`Hello, my name is`}</Typography>
+          <Typography variant="h5" color="#00ADB5">{`Hello, my name is`}</Typography>
           <Typography variant="h1" sx={{ fontWeight: '700' }}>
             Krzysztof Kudzia
           </Typography>
