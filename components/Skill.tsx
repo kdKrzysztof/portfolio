ee1a736db52f74a619fe60476bd4c68a450f715c
Krzysiek <supermax865@gmail.com>
2023-02-05 21:02:18 +0100

    added "map" for each skill

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -4,6 +4,7 @@ import { Typography, Box, Grid } from '@mui/material';
 import styles from '../styles/Skills.module.scss';
 import Image from 'next/image';
 import { Roboto, Merriweather, Poppins } from '@next/font/google';
+import { motion } from 'framer-motion';
 
 const roboto = Roboto({
   weight: '400',
@@ -20,33 +21,43 @@ const poppins = Poppins({
   subsets: ['latin']
 });
 
+const item = {
+  hidden: { y: 100, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
+
 const Skill = (props: any) => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'Center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        color: 'white',
-        // border: '1px solid #00ADB5',
-        borderRadius: '8px',
-        margin: '1rem',
-        padding: '.5rem',
-        transition: 'background-color 200ms',
-        // backgroundColor: 'rgb(0, 173, 181, 0.05)',
-        ':hover': {
-          backgroundColor: 'rgb(0, 173, 181, 0.05)',
-          transition: 'background-color 200ms'
-        }
-      }}
-      maxWidth="6rem"
-      maxHeight="6rem">
-      <Image src={props.ImageURL} alt="" className={styles.SkillImage} />
-      <Typography variant="subtitle1" style={poppins.style}>
-        {props.SkillName}
-      </Typography>
-    </Box>
+    <motion.div variants={item}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'Center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          color: 'white',
+          // border: '1px solid #00ADB5',
+          borderRadius: '8px',
+          margin: '1rem',
+          padding: '.5rem',
+          transition: 'background-color 200ms',
+          // backgroundColor: 'rgb(0, 173, 181, 0.05)',
+          ':hover': {
+            backgroundColor: 'rgb(0, 173, 181, 0.05)',
+            transition: 'background-color 200ms'
+          }
+        }}
+        maxWidth="6rem"
+        maxHeight="6rem">
+        <Image src={props.ImageURL} alt="" className={styles.SkillImage} />
+        <Typography variant="subtitle1" style={poppins.style}>
+          {props.SkillName}
+        </Typography>
+      </Box>
+    </motion.div>
   );
 };
 
--- pages/Skills.tsx
+++ pages/Skills.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Typography, Grid } from '@mui/material';
 import styles from '../styles/Skills.module.scss';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 import Skill from '../components/Skill';
 
@@ -27,11 +28,37 @@ const poppins = Poppins({
   subsets: ['latin']
 });
 
+const skillsObj: any = [
+  { imageURL: HTMLlogo, skillName: 'HTML' },
+  { imageURL: CSSlogo, skillName: 'CSS' },
+  { imageURL: MUIlogo, skillName: 'MaterialUI' },
+  { imageURL: JSlogo, skillName: 'JavaScript' },
+  { imageURL: TSlogo, skillName: 'TypeScript' },
+  { imageURL: REACTlogo, skillName: 'React' },
+  { imageURL: NEXTlogo, skillName: 'Next.JS' },
+  { imageURL: NODElogo, skillName: 'Node.JS' },
+  { imageURL: EXPRESSlogo, skillName: 'Express.JS' },
+  { imageURL: MONGODBlogo, skillName: 'MongoDB' },
+  { imageURL: MONGOOSElogo, skillName: 'Mongoose' },
+  { imageURL: MYSQLlogo, skillName: 'MySQL' },
+  { imageURL: MARIADBlogo, skillName: 'MariaDB' },
+  { imageURL: SEQUELIZElogo, skillName: 'Sequelize' }
+];
+
+const container = {
+  visible: {
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.2
+    }
+  }
+};
+
 const Skills = () => {
   return (
     <div className={styles.main}>
       <div className={styles.content}>
-        <div className={styles.leftSide}>
+        <motion.div className={styles.leftSide}>
           <Typography
             variant="h4"
             color="#00ADB5"
@@ -45,26 +72,37 @@ const Skills = () => {
             style={poppins.style}>
             {`I've worked with these technologies:`}
           </Typography>
-        </div>
-        <div className={styles.rightSide}>
-          <Skill ImageURL={HTMLlogo} SkillName="HTML" />
-          <Skill ImageURL={CSSlogo} SkillName="CSS" />
-          <Skill ImageURL={MUIlogo} SkillName="MaterialUI" />
-          <Skill ImageURL={JSlogo} SkillName="JavaScript" />
-          <Skill ImageURL={TSlogo} SkillName="TypeScript" />
-          <Skill ImageURL={REACTlogo} SkillName="React" />
-          <Skill ImageURL={NEXTlogo} SkillName="NextJS" />
-          <Skill ImageURL={NODElogo} SkillName="NodeJS" />
-          <Skill ImageURL={EXPRESSlogo} SkillName="Express.js" />
-          <Skill ImageURL={MONGODBlogo} SkillName="MongoDB" />
-          <Skill ImageURL={MONGOOSElogo} SkillName="Mongoose" />
-          <Skill ImageURL={MYSQLlogo} SkillName="MySQL" />
-          <Skill ImageURL={MARIADBlogo} SkillName="MariaDB" />
-          <Skill ImageURL={SEQUELIZElogo} SkillName="Sequelize" />
-        </div>
+        </motion.div>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className={styles.rightSide}>
+          {skillsObj.map((index: any) => {
+            return <Skill key={index} ImageURL={index.imageURL} SkillName={index.skillName} />;
+          })}
+          ;
+        </motion.div>
       </div>
     </div>
   );
 };
 
 export default Skills;
+
+{
+  /* <Skill ImageURL={HTMLlogo} SkillName="HTML" />
+<Skill ImageURL={CSSlogo} SkillName="CSS" />
+<Skill ImageURL={MUIlogo} SkillName="MaterialUI" />
+<Skill ImageURL={JSlogo} SkillName="JavaScript" />
+<Skill ImageURL={TSlogo} SkillName="TypeScript" />
+<Skill ImageURL={REACTlogo} SkillName="React" />
+<Skill ImageURL={NEXTlogo} SkillName="NextJS" />
+<Skill ImageURL={NODElogo} SkillName="NodeJS" />
+<Skill ImageURL={EXPRESSlogo} SkillName="Express.js" />
+<Skill ImageURL={MONGODBlogo} SkillName="MongoDB" />
+<Skill ImageURL={MONGOOSElogo} SkillName="Mongoose" />
+<Skill ImageURL={MYSQLlogo} SkillName="MySQL" />
+<Skill ImageURL={MARIADBlogo} SkillName="MariaDB" />
+<Skill ImageURL={SEQUELIZElogo} SkillName="Sequelize" /> */
+}
