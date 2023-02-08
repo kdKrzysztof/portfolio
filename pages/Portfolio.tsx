cc3bf7e6fe71c2e40e61de4f75eb7e50cd850c38
Krzysiek <supermax865@gmail.com>
2023-02-08 12:35:11 +0100

    feat: added portfolio page

--- pages/Portfolio.tsx
+++ pages/Portfolio.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import getLanguage from '@/components/GetLanguage';
+import styles from '@/styles/Portfolio.module.scss';
+import { motion } from 'framer-motion';
+import { Typography } from '@mui/material';
+
+import { Poppins } from '@next/font/google';
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin']
+});
+
+const container = {
+  visible: {
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.2
+    }
+  }
+};
 
 const Portfolio = () => {
+  let selectedLanguage = getLanguage();
   return (
-    <div>portfolio</div>
-  )
-}
+    <div className={styles.main}>
+      <div className={styles.content}>
+        <motion.div className={styles.leftSide}>
+          <Typography
+            variant="h4"
+            color="#00ADB5"
+            className={[styles.Title, styles.noselect].join(' ')}>
+            {selectedLanguage.MyWork.Title1}
+          </Typography>
+        </motion.div>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className={styles.rightSide}>
+          {/* {skillsObj.map((index: any) => {
+            return (
+              <Skill key={index.skillName} ImageURL={index.imageURL} SkillName={index.skillName} />
+            );
+          })} */}
+          ;
+        </motion.div>
+      </div>
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
