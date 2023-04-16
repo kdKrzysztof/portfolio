73698e7fe73bbaa8fa62c4be7b37a35a57cda004
Krzysiek <supermax865@gmail.com>
2023-04-16 11:57:15 +0200

    refactor: made keys unique in list component

--- components/List.tsx
+++ components/List.tsx
@@ -1,25 +1,30 @@
 import type { ListInterface } from '@/types';
 import type { FC } from 'react';
 import { Typography, Grid, List, ListItem } from '@mui/material';
+import { v4 as uuidv4 } from 'uuid';
 
 const ListComponent: FC<ListInterface> = ({ array }) => {
   return (
     <List sx={{ listStyleType: 'disc', pl: '1.65rem', pb: '0rem', color: '#00ADB5' }}>
       <>
         {array.map((el) => {
+          const uniqueKey = uuidv4();
           return (
             <ListItem
-              key={el.Title}
+              key={uniqueKey}
               sx={{ display: 'list-item', pl: '.5rem', pt: '0rem', pb: '0.5rem' }}>
-                
               {el.Title !== '' ? (
-                <Typography key={el.Title} variant="subtitle2" color="white" fontWeight="bold">
+                <Typography
+                  key={uniqueKey + '-title'}
+                  variant="subtitle2"
+                  color="white"
+                  fontWeight="bold">
                   {el.Title}
                 </Typography>
               ) : null}
 
               {el.Desc !== '' ? (
-                <Typography key={el.Title} variant="subtitle2" color="white">
+                <Typography key={uniqueKey + '-desc'} variant="subtitle2" color="white">
                   {el.Desc}
                 </Typography>
               ) : null}
