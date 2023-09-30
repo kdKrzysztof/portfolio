12e16c61859bfbec0c703e530cf163d3d68ba0bc
Krzysiek <57269917+kdKrzysztof@users.noreply.github.com>
2023-09-30 09:48:31 +0200

    refactor: change message to linkedin

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -6,7 +6,6 @@ import ContactType from '@/components/ContactOption';
 
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import MessageIcon from '@mui/icons-material/Message';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Contact = () => {
@@ -31,9 +30,9 @@ const Contact = () => {
           ImageIcon={<EmailIcon sx={{ fontSize: '2rem' }} />}
         />
         <ContactType
-          ContactName={'Messenger'}
-          ContactData={'m.me/100092339736369'}
-          ImageIcon={<MessageIcon sx={{ fontSize: '2rem' }} />}
+          ContactName={'LinkedIn'}
+          ContactData={'https://www.linkedin.com/in/kudzia/'}
+          ImageIcon={<LinkedInIcon sx={{ fontSize: '2rem' }} />}
         />
       </div>
     </div>
